9aee19f476aa43e2123f46ef2e13de9d517fa11c
AW <infor@alexander-werner.com>
2018-02-20 01:30:26 +0100

    tests with ramda

--- jobsAW/selectors/index.js
+++ jobsAW/selectors/index.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import R from 'ramda';
 import bug from '../../_libs/bug';
 
 // import getRichJobData from './getRichJobData';
@@ -12,11 +13,63 @@ const elemKey = 'city';
 const elemKeyFn = (elem) => elem[elemKey];
 const getElemByKey = elemKey => elem => elem[elemKey];
 
+// const inBucket = (dataToProcess, comparison, elemKeyOrFn) => {
+//     const filteredJobData = dataToProcess.reduce((buckets, elem) => {
+//         const key = typeof elemKeyOrFn === 'string' ? 
+//             elem[elemKeyOrFn] :
+//             elemKeyOrFn(elem);
+
+//         comparison.includes(key) && arrayEnter(buckets, key).push(elem);
+        
+
+//         return buckets;
+
+//     }, {});
+
+//         bug('inBucket::filteredJobData', filteredJobData)
+// }
+
+// var byGrade = R.groupBy((elem) => {
+//     var score = student.score;
+//     return score < 65 ? 'F' :
+//            score < 70 ? 'D' :
+//            score < 80 ? 'C' :
+//            score < 90 ? 'B' : 'A';
+// });
+
+
+const getCity = R.prop('city');
+// const otherProp = '_';
+
+// const groupByCities = selectedProps => R.groupBy((elem) => {
+//     const key = getCity(elem);
+
+//     return selectedProps.includes(key) ? 
+//         key : otherProp
+// });
+const shortGroup = R.groupBy(getCity);
+
+// const groupByCities = selectedProps => R.groupBy((elem) => {
+//     const key = getCity(elem);
+
+//     return selectedProps.includes(key) ? 
+//         key : otherProp
+// });
+
+const selectedCities = ['M', 'S']
+// const groupIt = groupByCities(selectedCities);
+
+// const filterSelectedCities = R.filter(R.propSatisfies(x => selectedCities.includes(x), elemKey))
+const pickSelectedCities = R.pick(selectedCities);
+
 const inBucket = (dataToProcess, comparison, elemKeyOrFn) => {
     const filteredJobData = dataToProcess.reduce((buckets, elem) => {
-        const key = typeof elemKeyOrFn === 'string' ? 
-            elem[elemKeyOrFn] :
-            elemKeyOrFn(elem);
+        // const key = typeof elemKeyOrFn === 'string' ? 
+        //     elem[elemKeyOrFn] :
+        //     elemKeyOrFn(elem);
+
+        const getItem = R.prop(elemKey)
+        const key = getItem(elem)
 
         comparison.includes(key) && arrayEnter(buckets, key).push(elem);
         
@@ -30,12 +83,24 @@ const inBucket = (dataToProcess, comparison, elemKeyOrFn) => {
 
 const getSelectedCities = (state) => state.ui.filter.city.sel;
 
+
 export const getJobData = createSelector(
     getRichJobData,
     getSelectedCities,
 
     (richJobData, selectedCities) => {
                                                                         bug('selectors::richJobData', richJobData)
+
+            // const grouped = groupIt(richJobData)
+            // bug('grouped', grouped)
+
+            const shortGrouped = shortGroup(richJobData);
+            bug('shortGrouped', shortGrouped)
+
+            // const shortGroupedAndFiltered = filterSelectedCities(shortGrouped);
+            const shortGroupedAndFiltered = pickSelectedCities(shortGrouped);
+            bug('shortGroupedAndFiltered', shortGroupedAndFiltered)
+
         // inBucket(richJobData, selectedCities, elemKey);
         // inBucket(richJobData, selectedCities, elemKeyFn);
         inBucket(richJobData, selectedCities, getElemByKey(elemKey));
@@ -46,6 +111,16 @@ export const getJobData = createSelector(
             // return true;
         });
                                                                 bug('selectors::filteredJobData', filteredJobData)
+
+
+                                                             
+                // var numbers = [1, 2, 3, 4];
+                // var transducer = R.compose(R.map(R.add(1)), R.take(2));
+                // var tRes = R.transduce(transducer, R.flip(R.append), [], numbers);
+
+                // bug('tRes', tRes)
+
+                                                                
         return filteredJobData;
         // return richJobData;
     }
